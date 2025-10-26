26814cfdf05c5d3b4312181a954cae06066926ae
dmitei <dima9990860349@icloud.com>
2025-10-26 13:28:35 +0300

    pars

---
+++ shabalin.dmitrii/F0/dictionary.hpp
@@ -0,0 +1,13 @@
+#ifndef DICTIONARY_HPP
+#define DICTIONARY_HPP
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+namespace shabalin
+{
+  using Dict = std::unordered_map< std::string, std::vector< std::string > >;
+  using Dicts = std::unordered_map< std::string, Dict >;
+}
+#endif
+
