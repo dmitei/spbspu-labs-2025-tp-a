14d3c9dd6102410948aa980e6c7ba9c4c4c13094
dmitei <dima9990860349@icloud.com>
2025-10-26 13:17:06 +0300

    + cpp added

---
+++ shabalin.dmitrii/F0/streamGuard.cpp
@@ -0,0 +1,18 @@
+#include "StreamGuard.hpp"
+
+shabalin::StreamGuard::StreamGuard(std::basic_ios< char > & stream):
+  stream_(stream),
+  fmt_(stream.flags()),
+  width_(stream.width()),
+  precision_(stream.precision()),
+  fill_(stream.fill())
+{}
+
+shabalin::StreamGuard::~StreamGuard()
+{
+  stream_.flags(fmt_);
+  stream_.width(width_);
+  stream_.precision(precision_);
+  stream_.fill(fill_);
+}
+
